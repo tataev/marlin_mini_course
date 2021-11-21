266f637eeb190a13f15666cd1c51c79ca2874ff7
Khizri Tataev <bodycat@gmail.com>
2021-11-21 18:40:04 +0300

    hw 13

--- task_13_handler.php
+++ task_13_handler.php
@@ -2,21 +2,7 @@
 session_start();
 $number = $_SESSION['info'];
 $submit = $_POST['submit'];
-if (isset($submit)) {
-    (!$number) ? ($number = 1) : $number++;
-}
+if (isset($submit)) {(!$number) ? ($number = 1) : $number++;}
     $_SESSION['info'] = $number;
     header('Location: task_13.php');
-//    exit;
 ?>
-
-<!--$int = 100;--><!--echo ($int == 100) ? "да, int = 100" : "нет, int не ровно 100";-->
-<!--expression ? true_value : false_value-->
-<!--
-if (!$number) {
-$number = 1;
-} else {
-$number++;
-}
- -->
-<!--(!$number)? $number=1 : $number++ -->
